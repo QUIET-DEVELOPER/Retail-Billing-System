a8d5e34d34c400b85684b0fc0058589dd494664a
MADDHURI VENU <venu.nlr001@gmail.com>
2025-10-24 16:31:19 +0530

    proj: Project setup

---
+++ Backend/retail_billing_system/src/main/java/com/quitedev/retail_billing_system/RetailBillingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.quitedev.retail_billing_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailBillingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailBillingSystemApplication.class, args);
+	}
+
+}
---
+++ Backend/retail_billing_system/src/test/java/com/quitedev/retail_billing_system/RetailBillingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quitedev.retail_billing_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailBillingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
